8d116eae02a14088f45ff65646c826627a38ef1e
小不点 <xiaobudian@localhost>
2026-05-24 03:38:15 +0800

    fix: 元宝记录改为跳转到我的页面

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -2,5 +2,9 @@ Page({
   goPage(e) {
     const url = e.currentTarget.dataset.url;
     wx.navigateTo({ url });
+  },
+
+  goGold() {
+    wx.switchTab({ url: '/pages/profile/index' });
   }
-})
+});
